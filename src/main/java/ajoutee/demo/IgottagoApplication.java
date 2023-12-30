855bbc0f805dda9df431c920d20dca4ba7bcd533
yoobinmion <yyb400@ajou.ac.kr>
2023-12-30 15:52:35 +0900

    initialize project

---
+++ src/main/java/ajoutee/demo/IgottagoApplication.java
@@ -0,0 +1,13 @@
+package ajoutee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgottagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgottagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajoutee/demo/IgottagoApplicationTests.java
@@ -0,0 +1,13 @@
+package ajoutee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgottagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
