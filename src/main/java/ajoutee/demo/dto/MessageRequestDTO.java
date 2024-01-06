668060c4d8f3e583010c76db397bde13d58d7170
yoobinmion <yyb400@ajou.ac.kr>
2024-01-06 16:02:32 +0900

    fcm 알림 v1

---
+++ src/main/java/ajoutee/demo/dto/MessageRequestDTO.java
@@ -0,0 +1,21 @@
+package ajoutee.demo.dto;
+
+import ajoutee.demo.domain.MessageRequest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class MessageRequestDTO {
+    private String title;
+    private String body;
+    private String targetToken;
+
+    public MessageRequest toEntity(){
+        return MessageRequest.builder()
+                .title(title)
+                .body(body)
+                .targetToken(targetToken)
+                .build();
+    }
+}
---
+++ src/main/java/ajoutee/demo/repository/TokenRepository.java
@@ -0,0 +1,10 @@
+package ajoutee.demo.repository;
+
+import ajoutee.demo.domain.MessageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<MessageRequest,Long> {
+    Optional<MessageRequest> findById(Long id);
+}
